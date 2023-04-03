2a262e08bc1ce8591847269c297a85d739e6aee7
wldbs98 <guswldbs98@gmail.com>
2023-04-03 20:54:58 +0900

    commit

---
+++ src/main/java/com/study/test/AutoAppConfig.java
@@ -0,0 +1,9 @@
+package com.study.test;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan
+@Configuration
+public class AutoAppConfig {
+}
